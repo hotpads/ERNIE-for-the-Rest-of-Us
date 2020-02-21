83fd1882034334583117828b10790f0a03b0f18f
winstonq <winstonq@hotpads.com>
2020-02-21 15:14:02 -0800

    update setup

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name='ernie4us',
-    version='0.88',
-    description="Tensorflow access helper for Baidu's ERNIE NLP model",
+    version='0.1.0',
+    description="Tensorflow accessible helper for Baidu's ERNIE NLP model",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=['tensorflow>=1.13.1', 'dataclasses'],
     url='https://github.com/hotpads/ERNIE-for-the-rest-of-us/',
     author='Winston Quock',
